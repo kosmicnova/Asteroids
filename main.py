76e776072a9452a13a132fb6b090e2763782470b
kosmicnova <zoeylzera@gmail.com>
2025-08-14 13:11:05 +0100

    player sprite rendered

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(
+        SCREEN_WIDTH / 2,
+        SCREEN_HEIGHT / 2
+    )
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,7 +24,10 @@ def main():
                 return
 
         screen.fill("black")
-        pygame.display.flip() 
+        player.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
